de1a7946afd6c3f00a0302c59d284c84d4e2aec3
mystic-murdoc <mur.aba16@yandex.ru>
2025-04-01 20:36:27 +0300

    Добавил новые файлы

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,4 +1,38 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import web.model.Car;
+import web.service.CarService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
 public class CarController {
+
+    private final CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+        System.out.println("CarService инжектирован: " + (carService != null));
+    }
+
+    @GetMapping("/cars")
+    public String showCars(@RequestParam(value = "count", required = false, defaultValue = "5") int count, Model model) {
+        model.addAttribute("cars", carService.getCars(count));
+        return "cars";
+    }
+
+    @RequestMapping("**")
+    @ResponseBody
+    public String handleUnknownRequest(HttpServletRequest request) {
+        return "Путь " + request.getRequestURI() + " не найден!";
+    }
 }
